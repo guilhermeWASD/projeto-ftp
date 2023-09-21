fa9ef60a7d7f57098029a556f60ea5bdc09e64e5
Guilherme Rodrigues <gui.rodrigues268@pm.me>
2023-09-20 21:49:03 -0300

    Atualizando banco de dados

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	define('HOST', 'localhost');
 	define('USER', 'root');
-	define('PASS', 'mansql2');
+	define('PASS', 'mansql1');
 	define('BASE', 'cadastro');
 
 	$conn = new MySQLi(HOST, USER, PASS, BASE);
